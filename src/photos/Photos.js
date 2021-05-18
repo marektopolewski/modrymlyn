d6d0423dc6987d4d207c3b2faa8aadf5a6d87493
Marek Topolewski <mt820@ic.ac.uk>
2021-05-18 15:32:49 +0200

    Make photos page modular for content

--- src/photos/Photos.js
+++ src/photos/Photos.js
@@ -12,6 +12,37 @@ import photo3 from '../assets/photo-3.jpeg'
 import './Photos.css'
 
 export default class Photos extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            headers: [
+                "Kaszubskie a <i style=\"color:green\" }}>Wege</i>",
+                "Sledze z pùlkama",
+                "W przeddzień otwarcia...",
+            ],
+            descriptions: [
+                [
+                    "Czy w kuchni kaszubskiej znajdzie się coś dla wegetarian? U nas - tak! 🥗",
+                    "Chrupiące kuleczki z kaszy gryczanej i boczniaka wypełnione kozim serem prosto z gospodarstwa \"Kaszubska Koza\" 🐐🐐🐐"
+                ],
+                [
+                    "Nasza narodowa specjalność - śledzie 🐟🐟🐟",
+                    "Mówi się, że kaszubskie kucharki potrafią przygotować je na 1000 sposobów!"
+                ],
+                [
+                    "Czy macie ochotę skosztować dań kuchni kaszubskiej w nowoczesnej odsłonie? 🌼",
+                    "Nasi kucharze przez wiele miesięcy poszukiwali receptur, składników i dostawców, aby stworzyć kartę dań, która przeniesie Was do magicznej krainy kaszubskich smaków 🍓"
+                ]
+            ],
+            images: [
+                photo3,
+                photo2,
+                photo1
+            ]
+        }
+    }
+
     render() {
         return (
             <Container className="wrapper">
@@ -19,34 +50,17 @@ export default class Photos extends React.Component {
                 nextIcon={<ArrowRightCircle color="black" size={50} className="svgShadow" />}
                 prevIcon={<ArrowLeftCircleFill color="black" size={50} className="svgShadow" />}
             >
-                <Carousel.Item>
-                    <Image className="d-block image" src={photo3} alt="Zdjęcie 1" fluid />
-                    <Carousel.Caption style={{ color:'black' }}>
-                        <h3>Kaszubskie a <i style={{ color:'green' }}>Wege</i></h3>
-                        <p>Czy w kuchni kaszubskiej znajdzie się coś dla wegetarian? U nas - tak! 🥗</p>
-                        <p>Chrupiące kuleczki z kaszy gryczanej i boczniaka wypełnione
-                           kozim serem prosto z gospodarstwa "Kaszubska Koza" 🐐🐐🐐</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <Image className="d-block image" src={photo2} alt="Zdjęcie 2" fluid />
-                    <Carousel.Caption style={{ color:'black' }}>
-                        <h3>Sledze z pùlkama</h3>
-                        <p>Nasza narodowa specjalność - śledzie 🐟🐟🐟</p>
-                        <p>Mówi się, że kaszubskie kucharki potrafią przygotować je na 1000 sposobów!</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <Image className="d-block image" src={photo1} alt="Zdjęcie 1" fluid />
-                    <Carousel.Caption style={{ color:'black' }}>
-                        <h3>W przeddzień otwarcia...</h3>
-                        <p>Czy macie ochotę skosztować dań kuchni kaszubskiej w nowoczesnej odsłonie? 🌼</p>
-                        <p>Nasi kucharze przez wiele miesięcy poszukiwali receptur, składników i dostawców,
-                           aby stworzyć kartę dań, która przeniesie Was do magicznej krainy kaszubskich smaków 🍓</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                { this.state.headers.map((_, i) => {
+                    return (
+                        <Carousel.Item>
+                            <Image className="d-block image" src={this.state.images[i]} alt={`Zdjęcie ${i}`} fluid />
+                            <Carousel.Caption style={{ color:'black' }}>
+                                <h3 dangerouslySetInnerHTML={{__html: this.state.headers[i]}}></h3>
+                                { this.state.descriptions[i].map(desc => { return <p>{desc}</p> }) }
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    )
+                }) }
             </Carousel>
             </Container>
         );
