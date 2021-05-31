274010d602174c8bbdb014586a4d1411cb6d859b
Marek Topolewski <mt820@ic.ac.uk>
2021-05-31 17:14:35 +0200

    Home: fix footer, add interior/exterior photos

--- src/home/Home.js
+++ src/home/Home.js
@@ -7,10 +7,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
-import logo from "../assets/mlyn_logo.jpg";
 import PigeonMap from './MapContainer';
-
+import logo from "../assets/mlyn_logo.jpg";
+import exterior from "../assets/photo-6.jpeg";
+import interior from "../assets/photo-6.jpeg";
 import './Home.css';
+
+const contentHeight = 300;
 
 const Home = () => (
     <>
@@ -32,33 +35,36 @@ const Home = () => (
     </Container>
 
     <Container>
-        <Row className="justify-content-md-center">
-            <Col><PigeonMap /></Col>
-            <Col>
-
+        <Row className="justify-content-center">
+            <Col className="content-col" xs="auto" sm="auto" md="auto" lg={4}>
+                <Image src={exterior} style={{ height: contentHeight }} fluid />
+            </Col>
+            <Col className="content-col" xs="auto" sm="auto" md="auto" lg={4}>
+                <Image src={interior} style={{ height: contentHeight }} fluid />
+            </Col>
+            <Col className="content-col" sm={12} md={8} lg={4}>
+                <PigeonMap height={contentHeight} />
             </Col>
         </Row>
     </Container>
 
-    <div className="footer">
-        <Container>
-            <Jumbotron>
-                <Row>
-                    <Col style={{ textAlign:'center' }}>
-                        <Row><Col className="phone-header">Zadzwoń do nas:</Col></Row>
-                        <Row><Col className="phone-number">(+48) 733 314 441</Col></Row>
-                    </Col>
-                    <Col sm>
-                        <Row style={{ textAlign:'center' }}>
-                            <Col><SocialIcon url="https://www.facebook.com/modrymlyn.rumia"/></Col>
-                            <Col><SocialIcon url="https://www.instagram.com/modrymlyn.rumia"/></Col>
-                            <Col><SocialIcon url="https://goo.gl/maps/Te9WZqxWGadQH34N7" network="google"/></Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Jumbotron>
-        </Container>
-    </div>
+    <Container style={{ marginTop:'2rem' }}>
+        <Jumbotron>
+            <Row>
+                <Col style={{ textAlign:'center' }}>
+                    <Row><Col className="phone-header">Zadzwoń do nas:</Col></Row>
+                    <Row><Col className="phone-number">(+48) 733 314 441</Col></Row>
+                </Col>
+                <Col sm>
+                    <Row style={{ textAlign:'center' }}>
+                        <Col><SocialIcon url="https://www.facebook.com/modrymlyn.rumia"/></Col>
+                        <Col><SocialIcon url="https://www.instagram.com/modrymlyn.rumia"/></Col>
+                        <Col><SocialIcon url="https://goo.gl/maps/Te9WZqxWGadQH34N7" network="google"/></Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Jumbotron>
+    </Container>
     </>
 );
 
--- src/home/MapContainer.js
+++ src/home/MapContainer.js
@@ -2,11 +2,17 @@ import React from "react"
 import { Map, Marker } from "pigeon-maps"
 
 export default class PigeonMap extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { height: props.height };
+    }
+
     render() {
         const lat = 54.57370106361447;
         const lng = 18.394757456212833;
         return (
-            <Map height={300} defaultCenter={[lat, lng]} defaultZoom={15}>
+            <Map height={this.state.height} defaultCenter={[lat, lng]} defaultZoom={15}>
                 <Marker width={50} anchor={[lat, lng]} />
             </Map>
         );
