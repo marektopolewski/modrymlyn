1618474737ab73a849306a76e07453ce625be361
Marek Topolewski <mt820@ic.ac.uk>
2022-04-17 12:42:07 +0200

    require photo json instead of import

--- src/photos/Photos.js
+++ src/photos/Photos.js
@@ -4,11 +4,17 @@ import Carousel from 'react-bootstrap/Carousel';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import { ArrowRightCircle, ArrowLeftCircleFill } from 'react-bootstrap-icons';
-
-import data from './photo-data.json';
 import './Photos.css'
 
 export default class Photos extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: require('./photo-data.json')
+        };
+    }
+
     render() {
         return (
             <Container className="wrapper">
@@ -16,11 +22,11 @@ export default class Photos extends React.Component {
                 nextIcon={<ArrowRightCircle color="black" size={50} className="svgShadow" />}
                 prevIcon={<ArrowLeftCircleFill color="black" size={50} className="svgShadow" />}
             >
-                { data.map((item, itemIdx) => {
+                { this.state.data.map((item, itemIdx) => {
                     return (
                         <Carousel.Item key={itemIdx}>
                             <Image className="d-block image" alt={`Zdjęcie ${itemIdx}`} fluid
-                                   src={require(`../assets/photo-${data.length - itemIdx}.jpeg`)?.default} />
+                                   src={require(`../assets/photo-${this.state.data.length - itemIdx}.jpeg`)?.default} />
                             <Carousel.Caption style={{ color:'black' }}>
                                 <h3 dangerouslySetInnerHTML={{__html: item.header}}></h3>
                                 {
