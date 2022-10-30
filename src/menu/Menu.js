e011b4bc2e7b7b099b30f11a8f454edada7ccca5
Marek Topolewski <marek.topolewski@gmail.com>
2022-10-30 13:53:34 +0100

    fix change lang button pos

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,29 +1,30 @@
+import { useState } from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 
+import './Menu.css';
+
 import logo2 from "../assets/mlyn_logo2.jpg";
 
-import menuPol1 from "../assets/menu-pol/menu-modry-mlin-pol_Page_2.jpg"
-import menuPol2 from "../assets/menu-pol/menu-modry-mlin-pol_Page_3.jpg"
-import menuPol3 from "../assets/menu-pol/menu-modry-mlin-pol_Page_4.jpg"
-import menuPol4 from "../assets/menu-pol/menu-modry-mlin-pol_Page_5.jpg"
-import menuPol5 from "../assets/menu-pol/menu-modry-mlin-pol_Page_6.jpg"
-import menuPol6 from "../assets/menu-pol/menu-modry-mlin-pol_Page_7.jpg"
-import menuPol7 from "../assets/menu-pol/menu-modry-mlin-pol_Page_8.jpg"
+import menuPol1 from "../assets/menu-pol/menu-modry-mlin-pol_Page_2.jpg";
+import menuPol2 from "../assets/menu-pol/menu-modry-mlin-pol_Page_3.jpg";
+import menuPol3 from "../assets/menu-pol/menu-modry-mlin-pol_Page_4.jpg";
+import menuPol4 from "../assets/menu-pol/menu-modry-mlin-pol_Page_5.jpg";
+import menuPol5 from "../assets/menu-pol/menu-modry-mlin-pol_Page_6.jpg";
+import menuPol6 from "../assets/menu-pol/menu-modry-mlin-pol_Page_7.jpg";
+import menuPol7 from "../assets/menu-pol/menu-modry-mlin-pol_Page_8.jpg";
 
-import menuEng1 from "../assets/menu-ang/menu-modry-mlin-ang_Page_2.jpg"
-import menuEng2 from "../assets/menu-ang/menu-modry-mlin-ang_Page_3.jpg"
-import menuEng3 from "../assets/menu-ang/menu-modry-mlin-ang_Page_4.jpg"
-import menuEng4 from "../assets/menu-ang/menu-modry-mlin-ang_Page_5.jpg"
-import menuEng5 from "../assets/menu-ang/menu-modry-mlin-ang_Page_6.jpg"
-import menuEng6 from "../assets/menu-ang/menu-modry-mlin-ang_Page_7.jpg"
-import menuEng7 from "../assets/menu-ang/menu-modry-mlin-ang_Page_8.jpg"
-
-import './Menu.css';
-import { useState } from 'react';
+import menuEng1 from "../assets/menu-ang/menu-modry-mlin-ang_Page_2.jpg";
+import menuEng2 from "../assets/menu-ang/menu-modry-mlin-ang_Page_3.jpg";
+import menuEng3 from "../assets/menu-ang/menu-modry-mlin-ang_Page_4.jpg";
+import menuEng4 from "../assets/menu-ang/menu-modry-mlin-ang_Page_5.jpg";
+import menuEng5 from "../assets/menu-ang/menu-modry-mlin-ang_Page_6.jpg";
+import menuEng6 from "../assets/menu-ang/menu-modry-mlin-ang_Page_7.jpg";
+import menuEng7 from "../assets/menu-ang/menu-modry-mlin-ang_Page_8.jpg";
 
 const MENU_POL = [ [menuPol1, menuPol2], [menuPol3, menuPol4], [menuPol5, menuPol6], [menuPol7]];
 const MENU_ENG = [ [menuEng1, menuEng2], [menuEng3, menuEng4], [menuEng5, menuEng6], [menuEng7]];
@@ -32,7 +33,9 @@ const Menu = () => {
     const [engMenu, setEngMenu] = useState(false);
     return (
         <Container className="menu">
-            <Button variant="outline-secondary" onClick={() => setEngMenu(eng => !eng)}>
+            <Button variant="outline-secondary" style={{ position: 'absolute' }}
+                onClick={() => setEngMenu(eng => !eng)}
+            >
                 {engMenu ? "Zmień język 🇵🇱" : "Change language 🇬🇧" }
             </Button>
             <Row className="justify-content-md-center">
