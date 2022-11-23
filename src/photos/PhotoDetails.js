081bdffbaff7a06d91f2b286a647f2ccffd3ec95
Marek Topolewski <marek.topolewski@gmail.com>
2022-11-23 21:12:23 +0100

    photo-details: remove sample text

--- src/photos/PhotoDetails.js
+++ src/photos/PhotoDetails.js
@@ -37,10 +37,6 @@ const PhotoDetails = () => {
                         <Card.Text key={paraIdx}>{para}</Card.Text>
                     ))
                 }
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
             </Card.Body>
         </Card>
         </Container>
